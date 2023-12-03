1ec8b32b5e9d7867b16d724f08238cd850e66808
crismdr <christina.modritski@gmail.com>
2023-12-03 14:23:17 +0200

    change dist

---
+++ dist/enums/personalInfo/personalInfo.d.ts
@@ -0,0 +1,21 @@
+export declare enum Rank {
+    turai = "\u05D8\u05D5\u05E8\u05D0\u05D9",
+    rabat = "\u05E8\u05D1\"\u05D8",
+    samal = "\u05E1\u05DE\u05DC",
+    samar = "\u05E1\u05DE\"\u05E8",
+    rasal = "\u05E8\u05E1\"\u05DC",
+    rasar = "\u05E8\u05E1\"\u05E8",
+    rasam = "\u05E8\u05E1\"\u05DD",
+    rasab = "\u05E8\u05E1\"\u05D1",
+    ranam = "\u05E8\u05E0\"\u05DD",
+    ranag = "\u05E8\u05E0\"\u05D2",
+    sagam = "\u05E1\u05D2\"\u05DD",
+    segen = "\u05E1\u05D2\u05DF",
+    seren = "\u05E1\u05E8\u05DF",
+    rasan = "\u05E8\u05E1\"\u05DF",
+    saal = "\u05E1\u05D0\"\u05DC",
+    alam = "\u05E1\u05D0\"\u05DC",
+    taal = "\u05EA\u05D0\"\u05DC",
+    aluf = "\u05D0\u05DC\u05D5\u05E3",
+    raal = "\u05E8\u05D0\"\u05DC"
+}
---
+++ dist/types/personalInfo/personalInfo.d.ts
@@ -0,0 +1,7 @@
+export interface PersonalInfo {
+    id: number;
+    personalNumber: string;
+    firstName: string;
+    lastName: string;
+    rank: number;
+}
